e5aea6ba4e38c075af7bc7c54eb71a16d7726dc9
Timur <galactictimur@gmail.com>
2022-10-20 22:35:00 +0300

    123

--- src/main/java/com/example/oop1/MyRectangle.java
+++ src/main/java/com/example/oop1/MyRectangle.java
@@ -1,20 +1,29 @@
 package com.example.oop1;
 
 
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
 import java.util.Random;
 
-public class MyRectangle implements Shape {
+public class MyRectangle  {
     private Rectangle rectangle;
+
+    private boolean isVisible = true;
     private double X;
     private double Y;
     private double translateX;
     private double translateY;
+    private int min = 2;
+    private int max = 5;
+    private int random = (int) Math.floor(Math.random() * (max - min + 1) + min);
+    private Rectangle[] rectangles = new Rectangle[random + 1];
 
-    private  String[] Colors = new String[] {"BLUE","RED","YELLOW","BLACK","GREEN","DARKRED","MAGENTA","CYAN","WHITE","GRAY","PINK","ORANGE"};
+    private  String[] Colors = new String[] {"BLUE","RED","YELLOW","BLACK","GREEN","DARKRED","MAGENTA","CYAN","GRAY","PINK","ORANGE"};
 
     public MyRectangle(double X, double Y, double translateX, double translateY) {
         this.X = X;
@@ -22,27 +31,30 @@ public class MyRectangle implements Shape {
         this.translateX = translateX;
         this.translateY = translateY;
     }
-
-    @Override
-    public void random() {
-        int randomX = new Random().nextInt(1000);
-        int randomY = new Random().nextInt(500);
-        int random = new Random().nextInt(Colors.length);
-        rectangle.setFill(Paint.valueOf(Colors[random]));
-        rectangle.setTranslateX(randomX);
-        rectangle.setTranslateY(randomY);
-    }
-
-    Rectangle create() {
-        rectangle = new Rectangle(X, Y);
-        rectangle.setTranslateX(translateX);
-        rectangle.setTranslateY(translateY);
-        Color color = Color.DARKRED;
-        rectangle.setFill(color);
-        return rectangle;
-    }
-
     Rectangle getRectangle() {
         return rectangle;
+    }
+    public void RectanglesRandM() {
+        int randomMove1 = (int) Math.floor(Math.random() * 41 - 20);
+        int randomMove2 = (int) Math.floor(Math.random() * 41 - 20);
+        try {
+            rectangle.setTranslateY(rectangle.getTranslateY() + randomMove1);
+            rectangle.setTranslateX(rectangle.getTranslateX() + randomMove2);
+        } catch (NullPointerException e) {}
+    }
+    public void show(AnchorPane anchorPane) {
+        if (isVisible == false) {
+            anchorPane.getChildren().add(rectangle);
+            isVisible = !isVisible;
+        } else {
+            anchorPane.getChildren().remove(rectangle);
+            isVisible = !isVisible;
+        }
+    }
+    public void create(AnchorPane anchorPane){
+        int random = new Random().nextInt(11);
+        rectangle = new Rectangle(X, Y, translateX, translateY);
+        rectangle.setFill(Paint.valueOf(Colors[random]));
+        anchorPane.getChildren().add(rectangle);
     }
 }
--- src/main/java/com/example/oop1/Shape.java
+++
@@ -1,8 +0,0 @@
-package com.example.oop1;
-
-public interface Shape {
-
-
-    void random();
-
-}
